6fcb973b334e2b8886aed298a8e646afba37ec7c
Robertoaclu <robertoaclu@gmail.com>
2023-11-09 15:59:13 +0100

    upgrade files

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const express = require ("express");
-const app = express ();
+const express = require("express");
+const app = express();
 const mysql = require("mysql2");
 
-app.use (express.static (`public`));
+app.use(express.static(`public`));
+app.use(express.json());
 
 // --------------------------------------------------------------------------
 // Crear conexion con MySQL
@@ -29,52 +30,90 @@ connection.connect(function (error) {
 // --------------------------------------------------------------------------
 // Aqui empieza la API
 
-
+// Ends points para INDEX
 // Carrusel
 
-app.get (`/carrusel`, function(request, response){
-    connection.query("select * from eventos", function (error, result, fields) {
+app.get(`/carrusel`, function (request, response) {
+    connection.query("SELECT * FROM eventos", function (error, result, fields) {
         if (error) {
             return console.error(`error: ${error.message}`);
         }
-        let eventos=[ ];
-        let total = 3;
+        let eventos = [];
+        let total = request.query.total;
 
-        for (let i = 0; i<total; i++){
+        for (let i = 0; i < total; i++) {
             eventos[i] = result[i];
         }
-        response.send(eventos);        
+        response.send(eventos);
     });
-    
+
 });
 
 
+app.get (`/eventos/:idEventos`, function(request, response) {
+    connection.query(`SELECT * FROM eventos WHERE id = "${request.params.idEventos}"`, function(error, result, fields) {
+        if (error) {
+            return console.error(`error: ${error.message}`);
+        }
+        let idEventos = request.params.idEventos;
+        
+        if (result.length==0){
+            response.send ({});
+        }else{
+            response.send(result[0]);
+        }
+        
+
+    });
+    
+    
+});
+// Termina INDEX -------------------------------------------------------------
+
+
+
+// Ends pints para login/registro
+// Ejemplo URL: http://localhost:8000/login?email=robertoaclu@gmail.com&password=1234
 // Login
 
-app.get (`/login`, function(request, response){
+app.get(`/login`, function (request, response) {
     const email = request.query.email;
     const password = request.query.password;
 
-    connection.query(`select * from usuarios where email = "${request.query.email}" and password = "${request.query.password}"`, function (error, result, fields) {
+    connection.query(`SELECT * FROM usuarios WHERE email = "${request.query.email}" and password = "${request.query.password}"`, function (error, result, fields) {
         if (error) {
             return console.error(`error: ${error.message}`);
         }
-        
-        if (result.length == 0){
-            response.send({message: "Email o password no validos"});
-        }else {
-            response.send({message: "Ususario logueado"});
+
+        if (result.length == 0) {
+            response.send({ message: "Email o password no validos" });
+        } else {
+            response.send({ message: "Ususario logueado" });
         }
-                
+
     });
-    
+
 });
+
+app.post(`/registro`, function(request, response){
+    let nombre= request.body.nombre;
+    let apellido= request.body.apellido;
+    let email= request.body.email;
+
+    console.log(request.body);
+
+    // insert into empleados_clientes
+    // insert into usuarios
+
+    response.send({message: `registro`});
+});
+//Termina login y registro --------------------------------------------------
 
 
 
 // --------------------------------------------------------------------------
 // Especificamos el puerto por el que se comunica el punto de salida
 
-app.listen(8000, function(){
+app.listen(8000, function () {
     console.log(`Server up and running!`);
 });
